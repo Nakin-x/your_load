558d415ffeaf51de6f6796349a74b8d80f9a995f
Nakin <andrea.caprari898@gmail.com>
2026-05-06 10:58:23 +0200

    modifica admin 1

--- api/admin.py
+++ api/admin.py
@@ -1,12 +1,6 @@
-from import_export.admin import ImportExportModelAdmin
 from django.contrib import admin
 from .models import Test, AppUser
 
-@admin.register(Test)
-class TestAdmin(ImportExportModelAdmin):
-    list_display = ('id', 'scheda_id', 'data', 'overall')
-    list_filter = ('scheda_id',)
-
-@admin.register(AppUser)
-class AppUserAdmin(ImportExportModelAdmin):
-    list_display = ('user_id', 'created_at')
+# Registrazione semplice, senza import-export
+admin.site.register(Test)
+admin.site.register(AppUser)
