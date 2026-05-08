d941fd6ad9d025a0b7ee8385430a4532643e3d90
Nakin <andrea.caprari898@gmail.com>
2026-05-08 11:49:13 +0200

    csv add 1

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Test, AppUser
+from import_export.admin import ExportMixin
 
-# Registrazione semplice, senza import-export
-admin.site.register(Test)
-admin.site.register(AppUser)
+@admin.register(Test)
+class YourModelAdmin(ExportMixin, admin.ModelAdmin):
+    pass
